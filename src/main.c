ca9910894a33afcd77c149eed114f49a585b700f
Aliahmad-Panda <141075686+Aliahmad-Panda@users.noreply.github.com>
2024-07-24 10:10:07 +0500

    Implementing a thread
    Implemented a thread to toggle a LED.

--- src/main.c
+++ src/main.c
@@ -5,10 +5,21 @@
  */
 
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+#include <inttypes.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+
+
+//For the thread
+#define STACK_SIZE 1024
+#define THREAD_PRIORITY 5
 
 #if !DT_NODE_EXISTS(DT_NODELABEL(load_switch))
 #error "Overlay for power output node not properly defined."
@@ -17,11 +28,33 @@
 #error "Overlay for power output node not properly defined."
 #endif
 
+//Thread structure and stack definition.
+K_THREAD_STACK_DEFINE(thread_stack, STACK_SIZE);
+struct k_thread thread_data;
+
+
 static const struct gpio_dt_spec load_switch =
 	GPIO_DT_SPEC_GET_OR(DT_NODELABEL(load_switch), gpios, {0});
 
 static const struct gpio_dt_spec another_switch =
 	GPIO_DT_SPEC_GET_OR(DT_NODELABEL(another_switch), gpios, {0});
+
+
+
+
+//Thread Function.
+void thread_function(void *unused1, void *unused2, void *unused3)
+{
+    while (1)
+    {
+        gpio_pin_set_dt(&load_switch, 1);
+        printf("Switching on(from thread)\n");
+        k_sleep(K_MSEC(1000));
+        gpio_pin_set_dt(&load_switch, 0);
+        printf("Switching off(from thread)\n");
+        k_sleep(K_MSEC(1000));
+    }
+}
 
 
 int main(void)
@@ -48,6 +81,9 @@ int main(void)
 		return 0;
 	}
 
+    k_tid_t my_tid = k_thread_create(&thread_data, thread_stack, STACK_SIZE, thread_function, NULL, NULL, NULL, THREAD_PRIORITY, 0, K_NO_WAIT);
+    k_thread_name_set(my_tid, "thread_function");
+
 	printf("Waiting one second.\n");
 
 	k_sleep(K_MSEC(1000));
@@ -61,8 +97,12 @@ int main(void)
 	while (1)
 	{
 		gpio_pin_set_dt(&another_switch, 1);
-		k_sleep(K_MSEC(1000));
+		printk("Switching on\n");
+        printf("Switching on(printf)\n");
+        k_sleep(K_MSEC(1000));
 		gpio_pin_set_dt(&another_switch, 0);
+        printk("Switching off\n");
+        printf("Switching off(printf)\n");
 		k_sleep(K_MSEC(1000));
 	}
 	
